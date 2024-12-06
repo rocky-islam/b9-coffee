46dc86031fcfa0e523e5b16ee700e9b8545ac024
Rocky islam <mohammadrockyislam01@gmail.com>
2024-12-06 16:45:24 +0600

    font added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ function App() {
     <>
       <div className="px-4 py-2">
         <button className="btn">Button</button>
-        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-neutral font-[Rancho] italic font-semibold text-2xl">Neutral</button>
         <button className="btn btn-primary">Primary</button>
         <button className="btn btn-secondary">Secondary</button>
         <button className="btn btn-accent">Accent</button>
         <button className="btn btn-ghost">Ghost</button>
         <button className="btn btn-link">Link</button>
-      </div>
+        <p className="font-[Rancho] text-6xl  font-semibold ">Espresso Emporium</p>
+    </div>
     </>
   );
 }
