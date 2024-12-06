b209247ca1c2cc2ae600f49602382c14488ccdad
Rocky islam <mohammadrockyislam01@gmail.com>
2024-12-06 16:09:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="px-4 py-2">
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
